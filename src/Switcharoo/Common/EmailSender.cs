b86c93dd87441daa15627ed1e76bb280a0d8a55a
Patric Green <bagro@bagro.se>
2024-01-01 15:25:08 +0100

    Add try catch

--- src/Switcharoo/Common/EmailSender.cs
+++ src/Switcharoo/Common/EmailSender.cs
@@ -10,20 +10,28 @@ public sealed class EmailSender(IOptions<SmtpSettings> options) : IEmailSender
 {
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        var smtpSettings = options.Value;
-        var mimeMessage = new MimeMessage();
-        mimeMessage.From.Add(new MailboxAddress(smtpSettings.SenderName, smtpSettings.Sender));
-        mimeMessage.To.Add(new MailboxAddress(email, email));
-        mimeMessage.Subject = subject;
-        mimeMessage.Body = new TextPart("html")
+        try
         {
-            Text = htmlMessage,
-        };
-        
-        using var smtpClient = new SmtpClient();
-        await smtpClient.ConnectAsync(smtpSettings.Host, smtpSettings.Port, smtpSettings.EnableSsl);
-        await smtpClient.AuthenticateAsync(smtpSettings.Username, smtpSettings.Password);
-        await smtpClient.SendAsync(mimeMessage);
-        await smtpClient.DisconnectAsync(true);
+            var smtpSettings = options.Value;
+
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress(smtpSettings.SenderName, smtpSettings.Sender));
+            mimeMessage.To.Add(new MailboxAddress(email, email));
+            mimeMessage.Subject = subject;
+            mimeMessage.Body = new TextPart("html")
+            {
+                Text = htmlMessage,
+            };
+
+            using var smtpClient = new SmtpClient();
+            await smtpClient.ConnectAsync(smtpSettings.Host, smtpSettings.Port, smtpSettings.EnableSsl);
+            await smtpClient.AuthenticateAsync(smtpSettings.Username, smtpSettings.Password);
+            await smtpClient.SendAsync(mimeMessage);
+            await smtpClient.DisconnectAsync(true);
+        }
+        catch (Exception ex)
+        {
+            // ignored for now
+        }
     }
 }
